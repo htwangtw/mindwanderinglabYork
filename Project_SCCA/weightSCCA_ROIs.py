76c7219156e4a6fb1b460b6cbe6ad5aced8b403c
Haoting <htwangtw@gmail.com>
2016-05-10 12:20:39 +0100

    bug fixed

--- Project_SCCA/weightSCCA_ROIs.py
+++ Project_SCCA/weightSCCA_ROIs.py
@@ -11,18 +11,29 @@ os.chdir(WD)
 rest_loading = np.load(expanduser('RSxMWQ_RS_SCCAloading_mat.npy'))
 # rest_comp = np.sum(rest_loading, axis=1)
 rest_comp = np.sum(rest_loading, axis=1)/np.sum(rest_loading!=0, axis=1)
-rest_comp[np.isnan(rest_comp)] = 0
+rest_comp [np.isnan(rest_comp)] = 0
 #load ROI masks
-ATLAS_DIR = 'C:\\Users\\hw1012\\Documents\\Project_CCA\\Bzdok_smoothed_DMN_subregions\\*.nii.gz'
+# ATLAS_DIR = 'C:\\Users\\hw1012\\Documents\\Project_CCA\\Bzdok_smoothed_DMN_subregions\\*fwhm8.nii.gz'
+ATLAS_DIR = 'C:\\Users\\hw1012\\Documents\\Project_CCA\\Bzdok_DMN\\*.nii.gz'
 atlas_nii = sorted(glob.glob(ATLAS_DIR)) #windows
+binarize = True
+# atlas_nii_re = []
+# atlas_nii_re.append(atlas_nii[0:2])
+# atlas_nii_re.append(atlas_nii[5:11])
+# atlas_nii_re.append(atlas_nii[2:5])
+# atlas_nii_re.append(atlas_nii[11:])
+# atlas_nii = [item for sublist in atlas_nii_re for item in sublist]
+
+
 sample_nii = nib.load('U:\\PhDProjects\\CS_Analysis\\CS_brain_preprocessed\\001_R4087_MNI152_2mm_prepro_filtered_func_data.nii.gz')
 weight_atlas = np.zeros(sample_nii.shape[:3])
 
 for k in range(rest_comp.shape[1]):
+# for k in range(2):
 	print 'component %i'%(k+1)
+	weight_atlas = np.zeros(sample_nii.shape[:3])
 	for i_roi, cur_roi in enumerate(atlas_nii):
 		print i_roi+1, cur_roi.split(os.sep)[-1].split('.nii.gz')[0], rest_comp[i_roi,k]
-		# binarize the data
 		cur_re_lable = resample_img(
 	            img=cur_roi,
 	            target_affine=sample_nii.get_affine(),
@@ -30,18 +41,18 @@ for k in range(rest_comp.shape[1]):
 	            interpolation='nearest'
 	        )
 		cur_data = cur_re_lable.get_data()
-		cur_data_bin = np.array(cur_data > 0.5, dtype=np.int)
-		cur_data = nib.load(cur_roi).get_data()
+		if binarize:
+			cur_data = np.array(cur_data > 0.5, dtype=np.int)
 		if len(cur_data.shape) > 3:
 			cur_data = cur_data[:, :, :, 0]
-		weight_atlas[cur_data > 0.1] = rest_comp[i_roi,k]
-
+		weight_atlas += cur_data * rest_comp[i_roi,k]
+		print np.sum(cur_data * rest_comp[i_roi,k])
 	#create labels
 	weight_atlas_nii = nib.Nifti1Image(
 	    weight_atlas,
 	    affine=sample_nii.get_affine(),
 	    header=sample_nii.get_header()
 	)
-	weight_atlas_nii.to_filename('weightedmask_comp%i.nii.gz'%(k+1))
+	weight_atlas_nii.to_filename('weightedmasks\\notsmoothed_weightedmask_comp%i.nii.gz'%(k+1))
 
 
