a73f7419b51b82cbd7625bb77eb48582e55018c4
Hao-Ting <haoting.wang@york.ac.uk>
2016-08-16 11:10:13 +0100

    annotation

--- TaskScripts/MindWanderingTask_psychopy_23062016/mindwandering_v2.01.py
+++ TaskScripts/MindWanderingTask_psychopy_23062016/mindwandering_v2.01.py
@@ -1,11 +1,16 @@
-expName = 'mindwandering_ageing' 
+expName = 'mindwandering_MDES' 
 #collect participant info, create logfile
 pptInfo = {
-    'subject': 'R0001_001', 
+    'subject': '001', 
     'session': '001', 
     'conditions' : ['random', '0-back', '1-back'],
     } #a pop up window will show up to collect these information
-
+"""
+conditions:
+random: randomly start with 1-back block or 0-back block
+0-back: start with 0-back block
+1-back: start with 1-back block 
+"""
 from psychopy import core
 
 from baseDef import*
@@ -16,7 +21,7 @@ expInfo, datafn = info_gui(expName, pptInfo)
 from MindWandering_TrialDisplay import*
 from MindWandering_StimList import*
 
-trials = getTrials(expInfo, datafn, switch=3)
+trials = getTrials(expInfo, datafn, switch=3) #3 switiches = four blocks = around 20 minutes 
 instruction()
 myClock = core.Clock()
 expTrial(myClock, trials, datafn, expInfo)
