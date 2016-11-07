23ed890189d5d4917979577cfd0412ca07dbbae3
Haoting <htwangtw@gmail.com>
2016-11-07 13:32:00 +0000

    SCCA using RPY2 syntax

--- custom_modules/scca_r.py
+++ custom_modules/scca_r.py
@@ -1,44 +1,23 @@
-import readline
 import rpy2.rinterface as ri
 import pandas as pd
-import pandas.rpy.common as com
-# load the Stanford package for SCCA, it should load dependent packages as well
-# please change the path.
-com.r(
-'''
-library(plyr)
-library(impute)
-library(Rcpp)
-library(PMA)
-''')
+from rpy2.robjects.packages import importr
+from rpy2.robjects import pandas2ri
+
+pandas2ri.activate()
+PMA = importr('PMA')
 
 def SCCA_r(X,Y, n_components, pen):
-
-
 	df_X = pd.DataFrame(X)
 	df_Y = pd.DataFrame(Y)
-
-	rmat_X = com.convert_to_r_matrix(df_X)
-	rmat_Y = com.convert_to_r_matrix(df_Y)
-
+	rmat_X = pandas2ri.py2ri(df_X)
+	rmat_Y = pandas2ri.py2ri(df_Y)
 	ri.globalenv['X'] = rmat_X
 	ri.globalenv['Y'] = rmat_Y
 
-	com.r(
-	    """
-	    out <- CCA(x = X, z = Y, K = %i, niter = 100, standardize = FALSE,
-	               penaltyx = %f, penaltyz = %f)
-	    """ % (n_components, pen[0], pen[1]))
-
-	# convert the results back to dataframes and then to numpy arrays
-	df_u = com.convert_robj(com.r('out[1]'))['u']
-	df_v = com.convert_robj(com.r('out[2]'))['v']
-	cors = com.convert_robj(com.r('out[16]'))['cors']
-
-	x_loadings = df_u.as_matrix()
-	y_loadings = df_v.as_matrix()
-	cors = np.array(cors)
+	out = PMA.CCA(x=X, z=Y, K=n_components, niter =100, standardize=False, penaltyx=pen[0], penaltyz=pen[1])
+	df_u = pandas2ri.ri2py(out[1])
+	df_v = pandas2ri.ri2py(out[2])
+	cors = pandas2ri.ri2py(out[15])
 	
-	loadings = (x_loadings, y_loadings)
-
+	loadings = (np.asmatrix(df_u), np.asmatrix(df_v))
 	return loadings, cors
